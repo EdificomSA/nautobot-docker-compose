44ad93c13fd22eb8bdcf489d7c63ba8e949a6eda
Yannis Ansermoz <yannis.ansermoz@edificom.ch>
2022-12-12 13:07:19 +0100

    add plugins

---
+++ nautobot_config.append.py
@@ -0,0 +1,42 @@
+PLUGINS = ["nautobot_device_onboarding"]
+#"nautobot_version_control"
+
+SECURE_SSL_REDIRECT = os.getenv("SECURE_SSL_REDIRECT", 'False').lower() == 'true'
+SESSION_COOKIE_SECURE = os.getenv("SESSION_COOKIE_SECURE", 'False').lower() == 'true'
+CSRF_COOKIE_SECURE = os.getenv("CSRF_COOKIE_SECURE", 'False').lower() == 'true'
+
+LOGLEVEL = os.getenv('LOGLEVEL', 'INFO')
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}',
+            'style': '{',
+        },
+        'simple': {
+            'format': '{levelname} {message}',
+            'style': '{',
+        },
+    },
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse',
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': LOGLEVEL,
+            'filters': ['require_debug_false'],
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+    },
+    'loggers': {
+        'django_auth_ldap': {
+            'handlers': ['console'],
+            'level': LOGLEVEL,
+        },
+    },
+}
